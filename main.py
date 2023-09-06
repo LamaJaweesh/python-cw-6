dc34e62d3972947f440dfb4cd152f35c66ed725a
LamaJaweesh <142673637+LamaJaweesh@users.noreply.github.com>
2023-09-06 19:43:30 +0300

    day6

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# write your code here
+person = { 
+  "name": "lama",
+  "age": 17,
+  "hobbies": ["reading", "volleyball", "arguing"]
+}
+print(person["name"])
+print(person["age"])
+
+person["age"] = 18
+person["country"] = "syria"
+
+print(person)
+print(len(person))
+person["hobbies"].append("cooking")
+
+def check_hobbies(person):
+    if len(person["hobbies"])> 3:
+        print("DAMNNN YOU ARE TALENTED")
+        
+check_hobbies(person)
